63137bbdb414cb615ff493b391898102b6a0515e
Supreme85 <romanyister@gmail.com>
2014-12-31 03:49:10 +0300

    Revert "Revert "Finish Test Data generator""
    This reverts commit 0acf001e997e4ef38bdefba15667d6bac7d44b86.

--- Java/src/main/java/TestsExample/SomeDataProvider.java
+++ Java/src/main/java/TestsExample/SomeDataProvider.java
@@ -5,6 +5,8 @@ import org.testng.annotations.DataProvider;
 
 import java.util.List;
 
+import static TestsExample.TestEnum.One;
+import static generator.VIDataGenerator.to2DArray;
 import static generator.ValuesGroup.ALL_GROUPS;
 
 /**
@@ -14,20 +16,50 @@ public class SomeDataProvider {
 
     @DataProvider(name = "someDP")
     public static Object[][] someDP() throws Exception {
-        List<Example> examplesDefaultC = new VIDataGenerator(Example.class).generateCombinations(ALL_GROUPS);
-        List<Example> examplesDefaultV = new VIDataGenerator(Example.class).generateValues(ALL_GROUPS);
-        List<Example> examplesAll = new VIDataGenerator(Example.class).generateCombinations(ALL_GROUPS);
-        List<Example> examples1w = new VIDataGenerator(Example.class).setWhiteList("subClass.Strings").generateCombinations(ALL_GROUPS);
-        List<Example> examples1b = new VIDataGenerator(Example.class).setBlackList("subClass.Strings").generateCombinations(ALL_GROUPS);
-        List<Example> examples1w2 = new VIDataGenerator(Example.class).setWhiteList("*.Strings").generateCombinations(ALL_GROUPS);
-        List<Example> examples1b2 = new VIDataGenerator(Example.class).setBlackList("*.Strings").generateCombinations(ALL_GROUPS);
+        List<Example> examplesDefaultC = new VIDataGenerator(Example.class)
+                .generateCombinations(ALL_GROUPS);
+        List<Example> examplesDefaultV = new VIDataGenerator(Example.class)
+                .generateValues(ALL_GROUPS);
+        List<Example> examplesAll = new VIDataGenerator(Example.class)
+                .generateCombinations(ALL_GROUPS);
+        List<Example> examples1w = new VIDataGenerator(Example.class)
+                .whiteList("subClass.Strings")
+                .generateCombinations(ALL_GROUPS);
+        List<Example> examples1b = new VIDataGenerator(Example.class)
+                .blackList("subClass.Strings")
+                .generateCombinations(ALL_GROUPS);
+        List<Example> examples1w2 = new VIDataGenerator(Example.class)
+                .whiteList("*.Strings")
+                .generateCombinations(ALL_GROUPS);
+        List<Example> examples1b2 = new VIDataGenerator(Example.class)
+                .blackList("*.Strings")
+                .generateCombinations(ALL_GROUPS);
 
         List examplesG1w = new VIDataGenerator(Example.class)
-                .setGenRule(b -> !b.toString().contains("subClass") || b.toString().contains("Enums")).generateCombinations(ALL_GROUPS);
+                .fieldsFilter(b -> { String B = b.toString();
+                    return !B.contains("subClass") || B.contains("Enums");} )
+                .generateCombinations(ALL_GROUPS);
+        List examplesValue1 = new VIDataGenerator(Example.class)
+                .dataFilter(e -> {
+                    Example E = (Example) e;
+                    return E.Enums == One && !E.subClass.Strings.equals("");
+                })
+                .generateCombinations(ALL_GROUPS);
+        List examplesValue2 = new VIDataGenerator(Example.class)
+                .dataFilter(e -> {
+                    Example E = (Example) e;
+                    return E.Enums != One || (E.Strings.equals(""));
+                })
+                .generateCombinations(ALL_GROUPS);
 
         List<Example> examples2 = new VIDataGenerator(Example.class).generateValues(ALL_GROUPS);
         List<Example> examples3 = new VIDataGenerator(Example.class).generateCombinations("Test");
+        return to2DArray(examples1w, Example.class);
+    }
+
+    @DataProvider(name = "someDP2")
+    public static Object[][] someDP2() throws Exception {
         List<Example> examples4 = new VIDataGenerator(Example.class).generateValues("Test2");
-        return new Object[][] {};
+        return to2DArray(examples4, Example.class);
     }
 }
--- Java/src/main/java/utils/PrintUtils.java
+++ Java/src/main/java/utils/PrintUtils.java
@@ -1,7 +1,14 @@
 package utils;
 
-import java.util.Arrays;
+import annotations.VIComplexData;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static java.lang.String.join;
+import static java.lang.reflect.Modifier.isStatic;
 import static utils.ReflectionUtils.getField;
 import static utils.ReflectionUtils.getFields;
 import static utils.LinqUtils.*;
@@ -14,7 +21,7 @@ public class PrintUtils {
     public static String print(Iterable<String> list) throws Exception { return print(list, ", ", "%s"); }
     public static String print(Iterable<String> list, String separator) throws Exception { return print(list, separator, "%s"); }
     public static String print(Iterable<String> list, String separator, String format) throws Exception {
-        return (list != null) ? String.join(separator, select(list, el -> format(format, el))) : "";
+        return (list != null) ? join(separator, select(list, el -> format(format, el))) : "";
     }
     public static String print(String[] list) throws Exception { return print(list, ", ", "%s"); }
     public static String print(String[] list, String separator) throws Exception { return print(list, separator, "%s"); }
@@ -28,4 +35,15 @@ public class PrintUtils {
             field -> field.getName() + ": '" + getField(field, obj) + "'"), separator, "%s");
         return format("%s(%s)", className, params);
     }
+
+    public static String printGroupValues(Object obj) throws Exception {
+        List<String> resultFields = new ArrayList<>();
+        for (Field field : obj.getClass().getFields())
+            if (!isStatic(field.getModifiers()))
+                resultFields.add(field.getAnnotation(VIComplexData.class) != null
+                    ? format("%s (%s)", field.getName(), field.get(obj).toString())
+                    : format("%s: %s", field.getName(), field.get(obj)));
+        return join("; ", resultFields);
+    }
+
 }
